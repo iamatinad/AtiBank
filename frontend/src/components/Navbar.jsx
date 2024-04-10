1a068118d7303e9a1343827f7e5b730472108f23
Atinadcodes <lamptey.danita@gmail.com>
2024-04-10 15:24:58 +0000

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import{ close, logo1, menu} from '../assets';
 import {navLinks} from '../constants';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
    <nav className="w-full flex py-6 justify-between items-center navbar">
     <img src={logo1} alt="atibank" className="w-[124px] h-[32px]"/>
@@ -22,6 +23,18 @@ const Navbar = () => {
       ))}
 
     </ul>
+    <div className="sm:hidden flex flex-1 justify-end items-center ">
+      <img src={toggle ? close :menu}
+      alt="menu"
+      className="w-[28px] h-[28px] object-contain"
+      onClick={() => setToggle((prev) => !prev)}
+      />
+<div className={`${toggle ? 'flex' : 'hidden'}`}
+>
+
+</div>
+
+    </div>
    </nav>
   )
 }
